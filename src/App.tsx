9028ccb373d6653edc5984fe76bc0a2aa3b6a75a
Wallace Júnior <wflj1997@hotmail.com>
2022-10-13 15:09:31 -0300

    register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Form } from "./components/Form";
+
 export const App: React.FC = () => (
-  <div className="min-h-screen flex items-center justify-center bg-zinc-900">
-    <h1 className="text-4xl text-zinc-100">Hello World</h1>
+  <div className="flex min-h-screen items-center justify-center bg-zinc-900 px-4">
+    <Form />
   </div>
 );
