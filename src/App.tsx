f8bac13fca48e2044a16cd87e356f5de2f272b79
Wallace Júnior <wflj1997@hotmail.com>
2022-10-13 13:53:36 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export const App: React.FC = () => (
+  <div className="min-h-screen flex items-center justify-center bg-zinc-900">
+    <h1 className="text-4xl text-zinc-100">Hello World</h1>
+  </div>
+);
